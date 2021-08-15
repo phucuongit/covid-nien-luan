35bedd20baf048c75cb5d3be999015848e1e6f8a
phucuongit <noname21062000@gmail.com>
2021-08-15 14:28:50 +0700

    add style lint scss

--- apps/admin-ui/.stylelintrc.js
+++
@@ -1,9 +0,0 @@
-const fabric = require("@umijs/fabric")
-
-module.exports = {
-  rules: {
-    "at-rule-no-unknown": null
-  },
-  ...fabric.stylelint,
-  ignoreFiles: ["src/assets/scss/**.scss"]
-}
