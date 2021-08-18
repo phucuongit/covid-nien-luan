043983766b98f19bc4dd291b6bf79a2ab4d97cea
phucuongit <noname21062000@gmail.com>
2021-08-18 18:12:08 +0700

    test rebuild

--- apps/admin-ui/src/App.vue
+++ apps/admin-ui/src/App.vue
@@ -2,7 +2,7 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-    <el-button type="danger">Danger test</el-button>
+    <el-button type="danger">Danger test12</el-button>
   </div>
   <router-view />
 </template>
