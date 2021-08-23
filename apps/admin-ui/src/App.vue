988f5419f382309f39087270f44e57972eba5996
buiyenlinh <buiyenlinh1406@gmail.com>
2021-08-23 19:33:34 +0700

    add ui-login

--- apps/admin-ui/src/App.vue
+++ apps/admin-ui/src/App.vue
@@ -1,9 +1,7 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-    <el-button type="danger">Danger</el-button>
-  </div>
+  <!-- <div id="nav"> -->
+    <!-- <router-link to="/">Home</router-link> -->
+  <!-- </div> -->
   <router-view />
 </template>
 
