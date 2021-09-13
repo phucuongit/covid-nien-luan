89d2b6d9a26a82a33d2db407dbbe508799035039
JC Naty <monnatyz@gmail.com>
2021-09-13 15:26:16 +0700

    update resource api

--- apps/admin-api/app/Models/Result_test.php
+++ apps/admin-api/app/Models/Result_test.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Traits\Filterable;
+use App\Models\User;
 
 class Result_test extends Model
 {
@@ -29,4 +30,14 @@ class Result_test extends Model
         'user_id',
         'create_by',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function user_create_by()
+    {
+        return $this->belongsTo(User::class, 'create_by', 'id');
+    }
 }
--- apps/admin-api/app/Models/Vaccination.php
+++ apps/admin-api/app/Models/Vaccination.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Traits\Filterable;
+use App\Models\User;
 
 class Vaccination extends Model
 {
@@ -30,4 +31,15 @@ class Vaccination extends Model
         'create_by',
         'vaccine_type_id',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function user_create_by()
+    {
+        return $this->belongsTo(User::class, 'create_by', 'id');
+    }
+
 }
