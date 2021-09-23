529f29ed26342bcc077605eb29d8f709ced6bcba
JC Naty <monnatyz@gmail.com>
2021-09-23 20:54:05 +0700

    update result test resource

--- apps/admin-api/app/Http/Resources/Result_testResource.php
+++ apps/admin-api/app/Http/Resources/Result_testResource.php
@@ -18,6 +18,7 @@ class Result_testResource extends JsonResource
     public function toArray($request)
     {
         $data = [
+            'status' => $this->status,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
