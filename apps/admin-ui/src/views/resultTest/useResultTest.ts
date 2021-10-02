04453e41c40b1798f04f008bf8d31082670086fb
buiyenlinh <buiyenlinh1406@gmail.com>
2021-10-02 11:56:51 +0700

    fix conflict

--- apps/admin-ui/src/views/resultTest/useResultTest.ts
+++ apps/admin-ui/src/views/resultTest/useResultTest.ts
@@ -59,41 +59,6 @@ function useResultTest() {
     }
   }
 
-  const searchResultTest = async (searchText: string) => {
-    try {
-      isLoadingSearch.value = true
-      isLoadingResultTestList.value = true
-      const response = await API.get("result_test?search=" + searchText)
-      if (response.data.success) {
-        resultTestList.value = response.data.data.result_tests
-        totalPage.value = response.data.data.meta.last_page
-      }
-    } catch (e) {
-      console.log(e)
-      isLoadingSearch.value = false
-      isLoadingResultTestList.value = false
-    } finally {
-      isLoadingSearch.value = false
-      isLoadingResultTestList.value = false
-    }
-  }
-
-  const filterResultTest = async (searchText: string) => {
-    try {
-      isLoadingResultTestList.value = true
-      const response = await API.get("result_test?search=" + searchText)
-      if (response.data.success) {
-        resultTestList.value = response.data.data.result_tests
-        totalPage.value = response.data.data.meta.last_page
-      }
-    } catch (e) {
-      console.log(e)
-      isLoadingResultTestList.value = false
-    } finally {
-      isLoadingResultTestList.value = false
-    }
-  }
-
   return {
     isLoadingResultTestList,
     resultTestList,
